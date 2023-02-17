49dd3ba5aa7dbc7f94fb55385124f3169a8e6c5b
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-17 22:57:37 +0200

    Values and Variables

--- 01-basics-part1/final/script.js
+++ 01-basics-part1/final/script.js
@@ -1,6 +1,23 @@
 let js = "amazing";
-if (js === "amazing") {
-  alert("Javascript is FUN!");
-}
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Vlad");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Developer";
+
+let job1 = "programmer";
+let job2 = "teacher";
