f379f1da2f96f987a4251e92e73a378616d84b99
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-18 18:38:19 +0200

    Strings and template literals

--- 01-basics-part1/final/script.js
+++ 01-basics-part1/final/script.js
@@ -95,6 +95,7 @@ const isFullAge = ageSarah >= 18;
 console.log(now - 1990 > now - 2018);
 */
 
+/*
 const now = 2023;
 const ageVlad = now - 1990;
 const ageSarah = now - 2018;
@@ -106,3 +107,64 @@ console.log(x, y);
 
 const averageAge = (ageVlad + ageSarah) / 2;
 console.log(ageVlad, ageSarah, averageAge);
+*/
+
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+
+/*  // SOLUTION 1
+// TEST DATA 1
+// let johnMass = 92;
+// let johnHeight = 1.95;
+// let markMass = 78;
+// let markHeight = 1.69;
+
+// TEST DATA 2
+let johnMass = 85;
+let johnHeight = 1.76;
+let markMass = 95;
+let markHeight = 1.88;
+
+const johnBMI = johnMass / johnHeight ** 2;
+const markBMI = markMass / (markHeight * markHeight);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI);
+console.log("Mark higher BMI = " + markHigherBMI); // TEST DATA 1 - true ; TEST DATA 2 - false
+*/
+
+const firstName = "Vlad";
+const job = "software developer";
+const birthYear = 1990;
+const year = 2023;
+
+const vlad =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "! ";
+console.log(vlad);
+
+const vladNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(vladNew);
+
+console.log(`Just a regular string...`);
+
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+console.log(`String 
+with
+multiple
+lines`);
