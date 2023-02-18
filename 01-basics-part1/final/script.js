873ee647c1c31836b236510e46803f030d69a496
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-18 20:08:16 +0200

    Truthy and Falsy values

--- 01-basics-part1/final/script.js
+++ 01-basics-part1/final/script.js
@@ -255,3 +255,26 @@ let n = "1" + 1;
 n = n - 1;
 console.log(n);
 */
+
+/*
+// 5 FALSY values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Vlad"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 123;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
