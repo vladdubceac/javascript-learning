4483fe9853a4efdfeb6aba82cfad524eb798a4eb
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-18 20:35:06 +0200

    Equality operators: == vs. ===

--- 01-basics-part1/final/script.js
+++ 01-basics-part1/final/script.js
@@ -278,3 +278,28 @@ if (height) {
   console.log("Height is UNDEFINED");
 }
 */
+
+const age = "18";
+if (age === 18) {
+  console.log(`You just became an adult :D (strict)`);
+}
+
+if (age == 18) {
+  console.log(`You just became an adult :D (loose)`);
+}
+
+const favourite = Number(prompt("What's your favourite number ? "));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
