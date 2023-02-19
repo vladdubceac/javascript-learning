4e22ba6bc629a37c1a0820402acfc87ef83be4a1
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-19 19:08:25 +0200

    challenge #1

--- 02-fundamentals-part2/final/script.js
+++ 02-fundamentals-part2/final/script.js
@@ -89,6 +89,7 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2, 3));
 */
 
+/*
 const calcAge = function (birthYear) {
   return 2023 - birthYear;
 };
@@ -107,3 +108,69 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1990, "Vlad"));
 console.log(yearsUntilRetirement(1950, "Mike"));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+// TEST DATA 1
+// const dolphinsScore1 = 44;
+// const dolphinsScore2 = 23;
+// const dolphinsScore3 = 71;
+// const koalasScore1 = 65;
+// const koalasScore2 = 54;
+// const koalasScore3 = 49;
+
+// TEST DATA 2
+const dolphinsScore1 = 85;
+const dolphinsScore2 = 54;
+const dolphinsScore3 = 41;
+
+const koalasScore1 = 23;
+const koalasScore2 = 34;
+const koalasScore3 = 27;
+
+const calcAverage = (a, b, c) => {
+  return (a + b + c) / 3;
+};
+
+const dolphinsAverage = calcAverage(
+  dolphinsScore1,
+  dolphinsScore2,
+  dolphinsScore3
+);
+const koalasAverage = calcAverage(koalasScore1, koalasScore2, koalasScore3);
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log(
+      `No team wins (Dolphins ${avgDolphins} vs. Koalas ${avgKoalas})`
+    );
+  }
+}
+
+checkWinner(dolphinsAverage, koalasAverage);
