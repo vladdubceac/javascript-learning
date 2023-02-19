aeddd7841bcd5cc9afa289068879b1f65ec647b2
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-19 09:12:22 +0200

    Ternary operator

--- 01-basics-part1/final/script.js
+++ 01-basics-part1/final/script.js
@@ -461,9 +461,29 @@ if (day === "monday") {
 }
 */
 
+/*
 if (23 > 10) {
   const str = "23 is bigger";
 }
 
 const me = "Vlad";
 console.log(`I'm ${2023 - 1990} years old ${me}`);
+*/
+
+const age = 23;
+// age >= 18
+//   ? console.log("I like to drink wine")
+//   : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
