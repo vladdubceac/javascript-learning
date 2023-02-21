20ba01b72997a72d2ee2b78221991de822802c56
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-21 20:51:21 +0200

    Object challenge

--- 02-fundamentals-part2/final/script.js
+++ 02-fundamentals-part2/final/script.js
@@ -347,6 +347,7 @@ console.log(
 );
 */
 
+/*
 const vlad = {
   firstName: "Vlad",
   lastName: "Dubceac",
@@ -385,3 +386,53 @@ console.log(vlad.age);
 // "Jonas is a 46-year old teacher, and he has no driver's license"
 
 console.log(vlad.getSummary());
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+if (john.calcBMI() > mark.calcBMI()) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+  );
+} else if (john.bmi < mark.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+  );
+} else {
+  console.log(
+    `${john.fullName} and ${mark.fullName} have equal BMIs (${john.bmi})`
+  );
+}
