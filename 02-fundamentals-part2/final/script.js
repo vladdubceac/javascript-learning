2e3dd02206542aa1f2bf0dfb6219ee1e21093ffb
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-21 22:43:38 +0200

    While loop

--- 02-fundamentals-part2/final/script.js
+++ 02-fundamentals-part2/final/script.js
@@ -492,6 +492,7 @@ for (let i = 0; i < vladArray.length; i++) {
 }
 */
 
+/*
 const vlad = [
   "Vlad",
   "Dubceac",
@@ -509,5 +510,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`--- Starting exercise ${exercise} ---`);
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise} , repetition ${rep}`);
+  }
+}
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log(`Loop is about to end...`);
   }
 }
