5edb0c53930f896fd6a2776df6fef7d49bd40094
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-24 23:20:19 +0200

    Debugging

--- 03-developer-skills/final/script.js
+++ 03-developer-skills/final/script.js
@@ -17,6 +17,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
+/*
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
@@ -39,6 +40,7 @@ const calcTempAmplitude = function (temps) {
 };
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
+*/
 
 // PROBLEM 2:
 // Function should now receive 2 arrays of temps
@@ -49,6 +51,7 @@ console.log(amplitude);
 // 2) Breaking up into sub-problems
 // - How to merge 2 arrays?
 
+/*
 const calcTempAmplitudeNew = function (t1, t2) {
   const temps = t1.concat(t2);
 
@@ -73,3 +76,53 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIX
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  console.log(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  //   let max = 0;
+  let max = temps[0];
+  //   let min = 0;
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') {
+      continue;
+    }
+
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY the bug
+console.log(amplitudeBug);
