ffa0b38c9bf34936d4bf6e079c40dac53e684db9
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-21 22:07:37 +0300

    Regular functions vs arrow functions

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -83,6 +83,7 @@ console.log(y === window.y);
 console.log(z === window.z);
 */
 
+/*
 console.log(this);
 
 const calcAge = function (birthYear) {
@@ -115,3 +116,51 @@ matilda.calcAge();
 
 const f = vlad.calcAge;
 f();
+*/
+
+// var firstName = 'Matilda';
+
+const vlad = {
+  firstName: 'Vlad',
+  year: 1990,
+  calcAge: function () {
+    // console.log(this);
+    console.log(new Date().getFullYear() - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && self.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+vlad.greet();
+vlad.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
