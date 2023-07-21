218245516e72973139a4cce4d4f68c48a1b52fa3
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-21 20:57:17 +0300

    Hoising and Temporal Dead Zone

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = new Date().getFullYear() - birthYear;
 
@@ -33,3 +34,49 @@ function calcAge(birthYear) {
 
 const firstName = 'Vlad';
 calcAge(1990);
+*/
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Vlad';
+let job = 'teacher';
+const year = 1990;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) {
+  deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
