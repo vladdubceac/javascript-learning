7de516988e9a807279122d396f6aaa5c7dab205d
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-21 20:20:04 +0300

    Scoping practice exercise

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -2,7 +2,32 @@
 
 function calcAge(birthYear) {
   const age = new Date().getFullYear() - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      const output = 'NEW OUTPUT';
+
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // add(2, 3);
+    console.log(output);
+  }
+  printAge();
   return age;
 }
 
