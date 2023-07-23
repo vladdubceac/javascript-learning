2e6358a8d3bca7c6f80bc94312c4eb92f351b00d
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-23 08:54:25 +0300

    Short circuiting (&& and ||)

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -47,6 +47,34 @@ const restaurant = {
   },
 };
 
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Vlad');
+console.log('' || 'Vlad');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Vlad');
+console.log(7 && 'Vlad');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
 /*
 //////////////////////////////
 // Rest Pattern and Parameters
