5201d8a391c434288c223ca5c804ec5527d74c1d
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-24 21:43:24 +0300

    Optional chaining (?.)

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -46,7 +46,43 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Vlad', email: 'hello@vlad.io' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) {
+  console.log(users[0].name);
+} else {
+  console.log(`user array empty`);
+}
+
 /*
+///////////////////////////////////////////
+// The for-of loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) {
