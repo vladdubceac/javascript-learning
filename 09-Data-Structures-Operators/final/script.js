0651c1b6bc4717fb93740f93c5b90ec1879c4488
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-27 21:45:25 +0300

    Working with Strings - Part 3

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -87,6 +87,60 @@ const game = {
   },
 };
 
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Vlad Dubceac'.split(' '));
+
+const [firstName, lastName] = 'Vlad Dubceac'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('vlad dubceac');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Vlad'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(68453546));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334843378463864647384'));
+
+// Repeat
+const message2 = 'Bad weather ... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line 
+${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+///////////////////////////////////////
+// Working with Strings - Part 2
 const airline = 'TAP Air Moldova';
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -141,6 +195,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some socks and a gun for protection');
+*/
 
 /*
 //////////////////////////////
