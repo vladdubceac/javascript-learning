af717a966c5acc6dc605948a23f35a1774cbf8cd
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-31 21:20:21 +0300

    The call and apply methods

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -126,6 +126,7 @@ biFunction(12, 0, division);
 biFunction(12, undefined, division);
 */
 
+/*
 // Functions returning functions
 const greet = function (greeting) {
   return function (name) {
@@ -143,3 +144,53 @@ greet('Hello')('Vlad');
 const greet1 = greeting => name => console.log(`${greeting}, ${name}`);
 
 greet1('Hi')('World');
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Vlad Dubceac');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sara Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
