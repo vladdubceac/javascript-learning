ed2c92533ba80109bbc38ddbc21ebdf68357431d
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-01 22:12:40 +0300

    Challenge #1 resolved

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -146,6 +146,7 @@ const greet1 = greeting => name => console.log(`${greeting}, ${name}`);
 greet1('Hi')('World');
 */
 
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -237,3 +238,110 @@ const addTaxRate = function (rate) {
 const addVAT2 = addTaxRate(0.2);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+*/
+
+//////////////////////////
+// Challenge #4
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an
+array with the number of replies for each option. This data is stored in the starter
+'poll' object below.
+Your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+method does 2 things:
+1.1. Display a prompt window for the user to input the number of the
+selected option. The prompt should look like this:
+What is your favourite programming language?
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+1.2. Based on the input number, update the 'answers' array property. For
+example, if the option is 3, increase the value at position 3 of the array by
+1. Make sure to check if the input is a number and if the number makes
+sense (e.g. answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string'
+or 'array'. If type is 'array', simply display the results array as it is, using
+console.log(). This should be the default option. If type is 'string', display a
+string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+object! So what should the this keyword look like in this situation?
+
+Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+Hints: Use many of the tools you learned about in this and the last section 😉
+GOOD LUCK 😀
+
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0,0,0,0]
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    let promptMessage = this.question;
+    for (let option of this.options) {
+      const [index, val] = option.split(':');
+      promptMessage += `\n${index} : ${val}`;
+    }
+
+    let availableAnswers = [];
+    for (let option of this.options) {
+      availableAnswers.push(option.slice(0, option.indexOf(':')));
+    }
+
+    const minAnswer = availableAnswers[0];
+    const maxAnswer = availableAnswers[availableAnswers.length - 1];
+
+    let answer;
+    do {
+      answer = Number(prompt(`${promptMessage}`));
+    } while (
+      (!answer && answer !== 0) ||
+      !(answer >= minAnswer && answer <= maxAnswer)
+    );
+    this.answers[answer]++;
+    this.displayResults();
+  },
+
+  displayResults: function (type = 'array') {
+    let output = '';
+    if (type === 'string') {
+      output = 'The results of the poll are ';
+      for (const [index, result] of this.answers.entries()) {
+        output += `${result}${index < this.answers.length - 1 ? ', ' : ''}`;
+      }
+    } else {
+      output = this.answers;
+    }
+    console.log(output);
+  },
+};
+
+// poll.registerNewAnswer();
+
+const pollRegisterNewAnswer = poll.registerNewAnswer;
+document
+  .querySelector('.poll')
+  .addEventListener('click', pollRegisterNewAnswer.bind(poll));
+
+const test = {};
+test.answers = [5, 2, 3];
+const testDisplayResults = poll.displayResults.bind(test);
+testDisplayResults();
+testDisplayResults('string');
+testDisplayResults('array');
+
+test.answers = [1, 5, 3, 9, 6, 1];
+testDisplayResults();
+testDisplayResults('string');
+testDisplayResults('array');
