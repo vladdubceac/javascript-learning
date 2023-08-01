9c7dab68790e21bae989c442586744b2da16adc2
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-01 23:25:14 +0300

    Closure examples

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -402,6 +402,7 @@ runOnce();
 console.log(notPrivate);
 */
 
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -417,3 +418,48 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log('TIMER');
+// }, 1000);
+
+const perGroup = 1000;
+boardPassengers(180, 3);
