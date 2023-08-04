b822451cb3ee0dcfe4e7a885086ad006ee5a8727
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-04 23:26:21 +0300

    Working with arrays. Challenge #2

--- 11-Array-Bankist/final/script.js
+++ 11-Array-Bankist/final/script.js
@@ -286,6 +286,8 @@ const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 */
 
+// The reduce() method
+/*
 console.log(movements);
 
 // accumulator -> SNOWBALL
@@ -306,3 +308,48 @@ const maximum = movements.reduce((acc, mov) => {
 }, movements[0]);
 
 console.log(maximum);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const dogsAges1 = [5, 2, 4, 1, 15, 8, 3];
+const dogsAges2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (dogAges) {
+  const humanAges = dogAges.map(dogAge =>
+    dogAge <= 2 ? dogAge * 2 : dogAge * 4 + 16
+  );
+
+  const filteredAges = humanAges.filter(age => age >= 18);
+
+  const average = filteredAges.reduce(
+    (acc, curr, _, arr) => acc + curr / arr.length,
+    0
+  );
+  return average;
+};
+
+console.log(dogsAges1);
+const averageAge1 = calcAverageHumanAge(dogsAges1);
+console.log(averageAge1);
+
+console.log(dogsAges2);
+const averageAge2 = calcAverageHumanAge(dogsAges2);
+console.log(averageAge2);
