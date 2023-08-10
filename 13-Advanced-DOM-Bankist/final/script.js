bb4fa4806ea01f9a44c4deebf45fc6c3660fdf34
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-10 21:42:30 +0300

    Implementing smooth scrolling

--- 13-Advanced-DOM-Bankist/final/script.js
+++ 13-Advanced-DOM-Bankist/final/script.js
@@ -35,7 +35,7 @@ document.addEventListener("keydown", function (e) {
 //////////////////////////
 ////////////////////
 ////////////////
-
+/*
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -118,4 +118,40 @@ logo.classList.toggle("c");
 logo.classList.contains("c");
 
 // Don't use
-// logo.className = "jonas";
+ logo.className = "jonas";
+*/
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log("Bounding client rectangle ", s1coords);
+
+  console.log(
+    "target.getBoundingClientRect() = ",
+    e.target.getBoundingClientRect()
+  );
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: "smooth",
+  });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
