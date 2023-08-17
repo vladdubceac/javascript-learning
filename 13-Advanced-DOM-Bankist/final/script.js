f89f81c83192f655176db0047db72c4884b1af67
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-17 22:46:29 +0300

    Revealing elements on scroll

--- 13-Advanced-DOM-Bankist/final/script.js
+++ 13-Advanced-DOM-Bankist/final/script.js
@@ -179,6 +179,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 //////////////////////////
 ////////////////////
 ////////////////
