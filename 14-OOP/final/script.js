5bee177b7efeab8bb8d12c7ac82197ec3c41f952
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-20 00:00:21 +0300

    Getters and setters

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -170,9 +170,10 @@ Mercedes.brake();
 // const PersonCl = class {};
 
 // class declaration
+
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -180,11 +181,30 @@ class PersonCl {
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name!`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -196,3 +216,23 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Class are first-class citizens
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'vlad',
+  movemements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movemements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movemements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movemements);
