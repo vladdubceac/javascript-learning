4ded7497b557975cfd758d8f1c88915dda4e646c
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-08-23 22:35:04 +0300

    Section 14 (OOP) . Challenge #4

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -585,6 +585,8 @@ jay.calcAge();
 // Public methods
 // Private methods
 // (there is also a static version)
+
+/*
 class Account {
   // 1) Public fields (instances)
   locale = navigator.language;
@@ -659,3 +661,102 @@ Account.helper();
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    if (charge > 100) {
+      this.#charge = 100;
+    } else if (charge < 0) {
+      this.#charge = 0;
+    } else {
+      this.#charge = charge;
+    }
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    if (this.#charge === 0) {
+      while (this.speed > 0) {
+        this.brake();
+      }
+      return;
+    }
+    if (this.#charge === 1) {
+      console.log(
+        `${this.make} cannot accelerate! The battery charge is ${
+          this.#charge
+        }% !`
+      );
+      return;
+    }
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+
+  brake() {
+    super.brake();
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
