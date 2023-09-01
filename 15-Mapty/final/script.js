731c6991ed18c01ff6049ec1cd1dea509d54c977
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-09-01 21:38:52 +0300

    Using the Geolocation API

--- 15-Mapty/final/script.js
+++ 15-Mapty/final/script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},19.17z?entry=ttu`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
