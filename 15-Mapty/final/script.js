0671cdf495d03129731b823669f485b33605adcd
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-09-01 22:50:28 +0300

    Displaying am Map using Leaflet library

--- 15-Mapty/final/script.js
+++ 15-Mapty/final/script.js
@@ -20,6 +20,20 @@ if (navigator.geolocation) {
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},19.17z?entry=ttu`
       );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer(`https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png`, {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
