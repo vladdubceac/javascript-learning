6adee8735720a5ce7e9471bb7d5f3770adca907e
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-09-14 22:20:41 +0300

    Asynchronous Javascript. Challenge #1

--- 16-Asynchronous/final/script.js
+++ 16-Asynchronous/final/script.js
@@ -5,16 +5,29 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const getPopulationText = function (population) {
+  let result;
+  if (population < 1000) {
+    result = `${population}`;
+  } else if (population < 1_000_000) {
+    result = `${(+population / 1000).toFixed(1)} thousand `;
+  } else {
+    result = `${(+population / 1_000_000).toFixed(2)} million `;
+  }
+  result += ` people`;
+  return result;
+};
+
 const renderCountry = function (data, className = '') {
   console.log(data);
+  const populationText = getPopulationText(data.population);
+
   const html = `<article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
             <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>👫</span>${populationText}</p>
             <p class="country__row"><span>🗣️</span>${
               data.languages[Object.keys(data.languages)[0]]
             }</p>
@@ -220,3 +233,56 @@ btn.addEventListener('click', function () {
 });
 
 // getCountryData('dsfddsfsdf');
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+const whereAmI = function (lat, long) {
+  fetch(` https://geocode.xyz/${lat},${long}?geoit=json`)
+    .then(res => {
+      if (!res.ok)
+        throw new Error(
+          `Problem with geocoding ${res.status} - ${res.statusText}`
+        );
+      return res.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      const url = `https://restcountries.com/v3.1/name//${data.country}`;
+      return fetch(url);
+    })
+    .then(res1 => {
+      if (!res1.ok) throw new Error(`Country not found (${res1.status})`);
+      return res1.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.err(err.message))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
