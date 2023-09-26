41bcd7436fe4eba624aca0dda423030d0efebcbd
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-09-26 22:14:49 +0300

    Returning values from Async functions

--- 16-Asynchronous/final/script.js
+++ 16-Asynchronous/final/script.js
@@ -500,9 +500,7 @@ const whereAmI = async function () {
     if (!resGeo.ok) {
       throw new Error('Problem getting location data');
     }
-
     const dataGeo = await resGeo.json();
-    console.log(dataGeo);
 
     // Country data
 
@@ -514,15 +512,36 @@ const whereAmI = async function () {
     }
 
     const data = await res.json();
-    console.log(data);
     renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
     console.error(`${err}`);
     renderError(`💥 ${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 };
-whereAmI();
-console.log('FIRST');
+
+console.log('1: Will get location');
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message}`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💥`);
+  }
+  console.log(`3: Finished getting location`);
+})();
 
 // try {
 //   let y = 1;
