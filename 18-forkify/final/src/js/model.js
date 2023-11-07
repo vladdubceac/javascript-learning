c5165e8a9af41004cc3ff2e476cd336081403b07
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-07 12:49:16 +0200

    Uploading a new recipe - Part 3

--- 18-forkify/final/src/js/model.js
+++ 18-forkify/final/src/js/model.js
@@ -1,6 +1,6 @@
 // import { async } from "regenerator-runtime";
 import { API_URL, RES_PER_PAGE, KEY } from "./config";
-import { getJSON, sendJSON } from "./helpers";
+import { AJAX } from "./helpers";
 
 export const state = {
   recipe: {},
@@ -30,7 +30,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await AJAX(`${API_URL}/${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some((bookmark) => bookmark.id === id)) {
@@ -46,7 +46,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
-    const data = await getJSON(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
     console.log(data);
 
     state.search.results = data.data.recipes.map((rec) => {
@@ -54,7 +54,8 @@ export const loadSearchResults = async function (query) {
         id: rec.id,
         title: rec.title,
         publisher: rec.publisher,
-        image: rec.image_url
+        image: rec.image_url,
+        ...(rec.key && { key: rec.key })
       };
     });
     state.search.page = 1;
@@ -130,7 +131,8 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter((entry) => entry[0].startsWith("ingredient") && entry[1] !== "")
       .map((ing) => {
-        const ingArr = ing[1].replaceAll(" ", "").split(",");
+        // const ingArr = ing[1].replaceAll(" ", "").split(",");
+        const ingArr = ing[1].split(",").map(el=>el.trim());
         if (ingArr.length !== 3)
           throw new Error(
             "Wrong ingredient format! Please use the correct format :)"
@@ -152,7 +154,7 @@ export const uploadRecipe = async function (newRecipe) {
     };
 
     console.log(recipe);
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmark(state.recipe);
   } catch (err) {
--- 18-forkify/final/src/js/views/previewView.js
+++ 18-forkify/final/src/js/views/previewView.js
@@ -21,6 +21,13 @@ class PreviewView extends View {
                         <p class="preview__publisher">${
                           this._data.publisher
                         }</p>
+                        <div class="preview__user-generated ${
+                          this._data.key ? `` : `hidden`
+                        }">
+                          <svg>
+                            <use href="${icons}#icon-user"></use>
+                          </svg>
+                        </div>
                       </div>
                     </a>
                   </li>`;
